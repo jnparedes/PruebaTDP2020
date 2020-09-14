6a9cce3e3a2c0689ce20997810fe0c390cad7a66
jnparedes <jnparedes91@gmail.com>
2020-09-14 20:59:23 -0300

    commit inicial

---
+++ Persona.java
@@ -0,0 +1,15 @@
+public class Persona{
+	private String name;
+
+	public Persona(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+}
---
+++ Prueba.java
@@ -0,0 +1,7 @@
+public class Prueba{
+
+	public static void main(String[] args){
+		Persona p = new Persona("Martin");
+		System.out.println("nombre: " + p.getName());
+	}
+}
