48a22f6aa4e3f88e5e5285e78fc6de081bc51d7a
jnparedes <jnparedes91@gmail.com>
2020-09-14 21:07:06 -0300

    segundo commit

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
 
 	public static void main(String[] args){
-		Persona p = new Persona("Martin");
+		Persona p = new Persona("Jose");
 		System.out.println("nombre: " + p.getName());
 	}
 }
